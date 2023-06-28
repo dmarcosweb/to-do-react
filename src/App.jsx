4e931492740dc42e5e14d5b048172639ec0d8827
Daniel mpm <dmarcosmpm.js@outlook.com>
2023-06-28 18:19:32 -0300

    → Listando Items ←

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Form from "./Form";
+import Items from './Items'
 import { nanoid } from "nanoid";
 
 const App = () => {
   const [items, setItems] = useState([]);
+  
   const addItem = (itemName) => {
     const newItem = {
       name: itemName,
@@ -13,9 +15,14 @@ const App = () => {
     setItems([...items, newItem]);
   };
 
+  const removeItem = () => {
+    alert('ok')
+  }
+
   return (
     <section className="section-center">
       <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} />
     </section>
   );
 };
